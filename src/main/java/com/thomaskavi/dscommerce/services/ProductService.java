454beca6e3d4968d36e15949d910f85a673fe19c
thomaskavi <thomaskavi2000@gmail.com>
2024-11-25 07:27:09 -0300

    Delete Product

--- src/main/java/com/thomaskavi/dscommerce/services/ProductService.java
+++ src/main/java/com/thomaskavi/dscommerce/services/ProductService.java
@@ -46,10 +46,16 @@ public class ProductService {
     return new ProductDTO(entity);
   }
 
+  @Transactional
+  public void delete(Long id) {
+    repository.deleteById(id);
+  }
+
   private void copyDtoToEntity(ProductDTO dto, Product entity) {
     entity.setName(dto.getDescription());
     entity.setDescription(dto.getDescription());
     entity.setPrice(dto.getPrice());
     entity.setImgUrl(dto.getImgUrl());
   }
+
 }
