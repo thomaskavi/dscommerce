84b72f56d7735fc974ec96fa4e70d4c15a08379c
thomaskavi <thomaskavi2000@gmail.com>
2024-12-02 06:42:06 -0300

    UserDetails, GrantedAuthority not tested

--- src/main/java/com/thomaskavi/dscommerce/entities/User.java
+++ src/main/java/com/thomaskavi/dscommerce/entities/User.java
@@ -2,9 +2,13 @@ package com.thomaskavi.dscommerce.entities;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -16,19 +20,10 @@ import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-@EqualsAndHashCode
 
 @Entity
 @Table(name = "tb_user")
-public class User {
+public class User implements UserDetails {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -52,32 +47,76 @@ public class User {
   @OneToMany(mappedBy = "client")
   private List<Order> orders = new ArrayList<>();
 
+  public User() {
+  }
+
+  public User(Long id, String name, String email, String phone, LocalDate birthDate, String password) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.phone = phone;
+    this.birthDate = birthDate;
+    this.password = password;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
   public void setId(Long id) {
     this.id = id;
+  }
+
+  public String getName() {
+    return name;
   }
 
   public void setName(String name) {
     this.name = name;
   }
 
+  public String getEmail() {
+    return email;
+  }
+
   public void setEmail(String email) {
     this.email = email;
+  }
+
+  public String getPhone() {
+    return phone;
   }
 
   public void setPhone(String phone) {
     this.phone = phone;
   }
 
+  public LocalDate getBirthDate() {
+    return birthDate;
+  }
+
   public void setBirthDate(LocalDate birthDate) {
     this.birthDate = birthDate;
+  }
+
+  public String getPassword() {
+    return password;
   }
 
   public void setPassword(String password) {
     this.password = password;
   }
 
+  public List<Order> getOrders() {
+    return orders;
+  }
+
   public void addRole(Role role) {
     roles.add(role);
+  }
+
+  public Set<Role> getRoles() {
+    return roles;
   }
 
   public boolean hasRole(String roleName) {
@@ -88,4 +127,40 @@ public class User {
     }
     return false;
   }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((id == null) ? 0 : id.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    User other = (User) obj;
+    if (id == null) {
+      if (other.id != null)
+        return false;
+    } else if (!id.equals(other.id))
+      return false;
+    return true;
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return roles;
+  }
+
+  @Override
+  public String getUsername() {
+    return email;
+  }
+
 }
