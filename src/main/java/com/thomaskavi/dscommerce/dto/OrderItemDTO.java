92ea00fdfa99c19fa9f906901ff3ee6b9a8f974e
thomaskavi <thomaskavi2000@gmail.com>
2024-12-03 13:45:42 -0300

    Add imgUrl to OrderItemDTO for product images

--- src/main/java/com/thomaskavi/dscommerce/dto/OrderItemDTO.java
+++ src/main/java/com/thomaskavi/dscommerce/dto/OrderItemDTO.java
@@ -12,14 +12,17 @@ public class OrderItemDTO {
 
   private Integer quantity;
 
+  private String imgUrl;
+
   public OrderItemDTO() {
   }
 
-  public OrderItemDTO(Long productId, String name, Double price, Integer quantity) {
+  public OrderItemDTO(Long productId, String name, Double price, Integer quantity, String imgUrl) {
     this.productId = productId;
     this.name = name;
     this.price = price;
     this.quantity = quantity;
+    this.imgUrl = imgUrl;
   }
 
   public OrderItemDTO(OrderItem entity) {
@@ -27,6 +30,7 @@ public class OrderItemDTO {
     name = entity.getProduct().getName();
     price = entity.getPrice();
     quantity = entity.getQuantity();
+    imgUrl = entity.getProduct().getImgUrl();
   }
 
   public Long getProductId() {
@@ -48,4 +52,9 @@ public class OrderItemDTO {
   public Double getSubTotal() {
     return price * quantity;
   }
+
+  public String getImgUrl() {
+    return imgUrl;
+  }
+
 }
